99618af7992c9f9c131c0ec829fa78f503220955
Matt McCormick <matt.mccormick@kitware.com>
2017-04-16 20:20:33 -0400

    Use CircleCI 2

--- centos-build.sh
+++ centos-build.sh
@@ -11,9 +11,11 @@ die() {
 export CC=/opt/rh/devtoolset-2/root/usr/bin/gcc
 export CXX=/opt/rh/devtoolset-2/root/usr/bin/g++
 
+mkdir -p /usr/src/CastXMLSuperbuild-build
 cd /usr/src/CastXMLSuperbuild-build || die "Could not cd into the build directory"
 
 cmake \
+  -DBUILD_FLAGS:STRING="-j4" \
   /usr/src/CastXMLSuperbuild || die "CMake configuration failed"
 make VERBOSE=1 || die "Build failed"
 cd castxml-prefix/src/castxml-build
@@ -21,3 +23,4 @@ ctest || die "CTest failed"
 
 cd /usr/src/CastXMLSuperbuild-build
 tar cvf castxml-linux.tar castxml
+gzip -9 castxml-linux.tar
